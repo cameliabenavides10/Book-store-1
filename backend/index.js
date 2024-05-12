d2f0877fa660eb895d667ad663672a2ecb386860
cameliabenavides10 <cameliabenavides@gmail.com>
2024-05-12 18:46:55 -0500

    successfully post book into database

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from  './models/books.js';
+
 const app = express();
+
+//middleware to parse the request body
+app.use(express.json());
+
 
 app.get('/', (request, response) => {
     console.log(request)
