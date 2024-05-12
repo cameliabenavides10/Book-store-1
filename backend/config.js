550f2118870d8c338085d87747cdfdd116db49ba
cameliabenavides10 <cameliabenavides@gmail.com>
2024-05-12 18:17:07 -0500

    successfully added mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT= 5555;
 
 export const mongoDBURL=
-'mongodb+srv://cameliabenavides10:<mongo>@book-store-mern.w1ikvte.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
+'mongodb+srv://cameliabenavides10:mongo@book-store-mern.w1ikvte.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
