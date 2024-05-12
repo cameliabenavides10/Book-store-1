2da1cb30c0c7ab6c41ac3a02790e82d0fe34eacc
cameliabenavides10 <cameliabenavides@gmail.com>
2024-05-12 18:36:41 -0500

    created a post route handler

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,33 @@ app.get('/', (request, response) => {
     return response.status(234).send('My Book Store App')
 });
 
+// route to save a new book
+app.post('/books', async (request, response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = { 
+            title: request.body.title, 
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
 
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+        
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 
 mongoose
